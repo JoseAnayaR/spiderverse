ca582e08f5f2de4d01f24a58eee4190d9f0714ff
JoseAnayaR <joseanayautj@gmail.com>
2022-05-02 13:04:31 -0500

    Create spiderman class

---
+++ app/spiderman.js
@@ -0,0 +1,10 @@
+class spiderman {
+    constructor(name, age, actor, numberOfMovies, studio){
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.numberOfMovies = numberOfMovies
+        this.studio = studio
+    }
+}
+module.exports = spiderman
---
+++ index.js
@@ -0,0 +1,3 @@
+const spiderman = require('./app/spiderman');
+const andrewGarfield = new spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+console.log(andrewGarfield)
